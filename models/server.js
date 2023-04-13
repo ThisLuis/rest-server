b9ee088a76a7fe54c059341a4e01d3978f87dc10
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-04-12 20:42:56 -0600

    Creamos un archivo para controlador y otro para las routes, separamos la rutas y el controlador de la clase

--- models/server.js
+++ models/server.js
@@ -5,7 +5,8 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT
+        this.port = process.env.PORT;
+        this.usersPath = '/api/users';
 
 
         // ? Middlewares
@@ -21,35 +22,7 @@ class Server {
     }
 
     routes() {
-        this.app.get('/api', (req, res) => {
-            res.json({
-                msg: 'get API'
-            });
-        });
-
-        this.app.put('/api', (req, res) => {
-            res.status(500).json({
-                msg: 'put API'
-            });
-        });
-
-        this.app.post('/api', (req, res) => {
-            res.json({
-                msg: 'post API'
-            });
-        });
-
-        this.app.delete('/api', (req, res) => {
-            res.json({
-                msg: 'delete API'
-            });
-        });
-
-        this.app.patch('/api', (req, res) => {
-            res.json({
-                msg: 'patch API'
-            });
-        });
+        this.app.use( this.usersPath, require('../routes/user'));
     }
 
     listen() {
---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const { usersGet, usersPut, usersPost, usersPatch, usersDelete } = require('../controllers/users');
+
+const router = Router();
+
+router.get('/', usersGet);
+
+router.put('/', usersPut);
+
+router.post('/', usersPost);
+
+router.delete('/', usersDelete);
+
+router.patch('/', usersPatch);
+
+module.exports = router;
