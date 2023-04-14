ddf7e408e980f25d4a7a04df654b1532646faccc
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-04-13 22:32:15 -0600

    Definimos el modelo para el usuario, solo creamos el schema, falta probar que funcione correctamente

---
+++ models/user.js
@@ -0,0 +1,45 @@
+// {
+//     name: 'name',
+//     email: 'mail@mail.com',
+//     password: '3213213131',
+//     img: 'imagen.jpg',
+//     role: 'rol',
+//     state: false,
+//     google: false
+// }
+
+const { Schema, model, models } = require('mongoose');
+
+const UserSchema = Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    img: {
+        type: String,
+    },
+    role: {
+        type: String,
+        required: [true, 'Role is required'],
+        emun: ['ADMIN_ROLE', 'USER_ROLE'],
+    },
+    state: {
+        type: Boolean,
+        default: true,
+    },
+    google: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+module.exports = model( 'User', UserSchema );
