d47923332fb89601b82eb704b165fa4c7805f320
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-04-18 12:48:38 -0600

    Creamos un middleware para validar todos los campos

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,5 @@
 const { response } = require('express');
 const bcryptjs = require('bcryptjs');
-const { validationResult }  = require('express-validator');
 
 const User = require('../models/user');
 const { exists } = require('../models/user');
@@ -19,10 +18,7 @@ const usersGet = ( req, res = response ) => {
 
 const usersPost = async ( req, res = response ) => {
 
-    const errors = validationResult( req );
-    if( !errors.isEmpty()) {
-        return res.status(400).json(errors);
-    }
+    
     
     const { name, email, password, role } = req.body;
     const user = new User({ name, email, password, role});
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const { usersGet,
         usersPost, 
         usersPatch, 
         usersDelete } = require('../controllers/users');
+const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
@@ -13,7 +14,11 @@ router.get('/', usersGet);
 router.put('/:id', usersPut);
 
 router.post('/', [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('password', 'El password debe ser de al menos 6 caracteres').isLength({ min: 6 }),
         check('email', 'El correo ingresado no es valido').isEmail(),
+        check('role', 'No es un rol permitido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+        validateFields,
 ]
 ,usersPost);
 
