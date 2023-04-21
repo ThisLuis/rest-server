e36517265f835e5f1f2f6a896849fefa51b569e0
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-04-21 17:50:21 -0600

    Validamos el user y el password en el controller auth para verificar que los datos sean correctos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,48 @@
 const { response } = require("express");
+const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
 
-const login = (req, res = response) => {
-    res.json({
-        msg: 'Login ok'
-    });
+const login = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        const user = await User.findOne({ email });
+
+        if( !user ){
+            return res.status(400).json({
+                msg: 'El email no se encuentra en la base de datos'
+            });
+        }
+
+        // Si el usuario esta activo
+        if ( !user.state ) {
+            return res.status(400).json({
+                msg: 'El usuario esta inactivo en la base de datos'
+            });
+        }
+
+        // Verificar la contraseña
+        const validPassword = bcryptjs.compareSync( password, user.password);
+        if ( !validPassword ) {
+            res.status(400).json({
+                msg: 'La contraseña no es correcta'
+            });
+        }
+        
+
+        res.json({
+            msg: 'Login OK'
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
+
 }
 
 module.exports = {
