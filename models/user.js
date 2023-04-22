279992227c62e2b95d1b7f556f000d0e42f98817
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-04-21 20:19:25 -0600

    Cambiamos visualmente el _id por uid

--- models/user.js
+++ models/user.js
@@ -44,7 +44,8 @@ const UserSchema = Schema({
 
 // Remover los campos __v y password modificando el metodo toJSON, solo mandamos los restantes a user
 UserSchema.methods.toJSON = function() {
-    const { __v, password, ...user } = this.toObject();
+    const { __v, password, _id, ...user } = this.toObject();
+    user.uid = _id;
     return user;
 }
 
