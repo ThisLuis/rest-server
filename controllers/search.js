f8b7408593b3019b5af68d52b5e696905982fc5d
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-05-03 17:51:15 -0600

    Agregamos una funcion que busca usuarios, necesitamos el nombre de la coleccion y el temrino de busqueda

--- controllers/search.js
+++ controllers/search.js
@@ -1,14 +1,54 @@
 const { response } = require('express');
+const { ObjectId } = require('mongoose').Types;
+
+const { Categorie, Product, User } = require('../models');
+
+const ALLOWED_COLLECTIONS = [
+    'categories',
+    'products',
+    'roles',
+    'users'
+];
+
+const searchUsers = async( query = '', res = response ) => {
+    const isMongoId = ObjectId.isValid( query );
+
+    if ( isMongoId ) {
+        const user = await User.findById( query );
+        res.json({
+            results: ( user ) ? [ user ] : []
+        });
+    }
+}
 
 const search = ( req, res = response ) => {
     
     const { collection, query } = req.params;
 
+    if( !ALLOWED_COLLECTIONS.includes( collection )) {
+        res.status(400).json({
+            msg: `Las colecciones permitidas son: ${ ALLOWED_COLLECTIONS }`
+        });
+    }
 
-    res.json({
-        collection,
-        query
-    })
+    switch( collection ){
+        case 'users':
+            searchUsers( query, res );
+        break;
+
+        case 'categorie':
+
+        break;
+
+        case 'products':
+        break;
+
+        default:
+            res.status(500).json({
+                msg: 'No se puede realizar esta busqueda...aun'
+            })
+
+    }
 }
 
 module.exports = {
