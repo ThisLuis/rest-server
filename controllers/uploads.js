88296b0921e218ba45ad51bf4d18719edce02ebb
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-05-03 21:31:21 -0600

    Creamos la validacion para asegurarnos que el archivo que se suba sea permitido

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -15,18 +15,30 @@ const uploadFile = (req, res = response) => {
         res.status(400).json({msg: 'No files were uploaded.'});
         return;
     }
-  
+    
     const { file } = req.files;
+    const nameCut = file.name.split('.');
+    const extension = nameCut[ nameCut.length - 1 ];
+    
+    // Valida la extension
+    const VALID_EXTENSIONS = [ 'png', 'jpg', 'jpeg', 'gif' ];
+    if( !VALID_EXTENSIONS.includes( extension )) {
+        res.status(400).json({
+            msg: `La extension: ${ extension } no esta permitida, ${ VALID_EXTENSIONS }`
+        })
+    }
+
+    res.json({ extension});
     // file.name es el nombre que tiene el archivo cuando lo subimos
-    const uploadPath = path.join( __dirname, '../uploads/', file.name);
+    // const uploadPath = path.join( __dirname, '../uploads/', file.name);
   
-    file.mv(uploadPath, (err) => {
-      if (err) {
-        return res.status(500).json( { err } );
-      }
+    // file.mv(uploadPath, (err) => {
+    //   if (err) {
+    //     return res.status(500).json( { err } );
+    //   }
   
-      res.json({ msg: 'El archivo se subio a: ' + uploadPath});
-    });
+    //   res.json({ msg: 'El archivo se subio a: ' + uploadPath});
+    // });
 }
 
 module.exports = {
