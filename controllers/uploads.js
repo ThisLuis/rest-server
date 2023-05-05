d1154514f4efa8d047ae43f35c6cb138042ad6ff
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-05-04 20:36:15 -0600

    Actualizamos la imagen del producto y del usuario, funciona bien, falta validar que no se quede pegado el server cuando no mandamos el file

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,6 +1,8 @@
 const { response }= require('express');
 const { uploadFile } = require('../helpers');
 
+const { Product, User } = require('../models');
+
 const uploadFiles = async(req, res = response) => {
   
     // Verifica que vengan los files - o - Verifica haciendo un barrido si viene al menos una propiedad files en la request
@@ -26,9 +28,41 @@ const uploadFiles = async(req, res = response) => {
 }
 
 const updateImage = async( req, res = response ) => {
+
     const { id, collection } = req.params;
 
-    res.json({ id, collection });
+    let model;
+
+    switch( collection ) {
+        case 'products':
+            model = await Product.findById( id );
+            if( !model ) {
+                return res.status( 400 ).json({
+                    msg: `No existe un producto con el id ${ id }`
+                });
+            }
+        break;
+            
+        case 'users':
+            model = await User.findById( id );
+            if ( !model ) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${ id }`
+                })
+            }
+        break;
+        default:
+            res.status(500).json({ msg: 'No esta validado...aun'})
+    }
+
+    const name = await uploadFile( req.files, undefined, collection );
+    model.img = name;
+
+    await model.save()
+
+
+
+    res.json( model );
 }
 
 module.exports = {
