b43f70118acbadfa81c06e34d7af9d09cdcd48ba
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-05-30 21:12:20 -0600

    Creamos el diseño basico del login y validamos su funcionamiento, ya podemos generar el JWT, falta validarlo

--- public/js/auth-google.js
+++ public/js/auth-google.js
@@ -15,8 +15,12 @@ form.addEventListener('submit', e => {
         headers: { 'Content-Type': 'application/json' }
     })
     .then( resp => resp.json())
-    .then( data => {
-        console.log(data)
+    .then( ( msg, token) => {
+        if( msg ) {
+            return console.error( msg );
+        }
+
+        localStorage.setItem('token', token)
     })
     .catch( err => {
         console.log(err)
